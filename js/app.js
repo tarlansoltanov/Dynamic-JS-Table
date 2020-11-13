d8667e0c0b82f183dc3eeed96dff2f1278421545
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2020-11-13 22:38:47 +0400

    Table add entry ready

--- js/app.js
+++ js/app.js
@@ -25,9 +25,13 @@ class Person {
     }
 }
 
+const addBtn = document.querySelector('#add');
+
 const entries = document.querySelector('tbody');
 
-person_1 = new Person('Tarlan', 'Soltanov', 'tarlan@example.com', '16')
+var check = true;
+
+person_1 = new Person('Tarlan', 'Soltanov', 'tarlan@example.com', '16');
 
 users.push(person_1);
 
@@ -35,4 +39,44 @@ for (var i = 0; i < users.length; i++) {
     var row = document.createElement('tr');
     row.innerHTML = users[i].info(i);
     entries.appendChild(row);
+}
+
+addBtn.addEventListener("click", () => {
+    if (check == false) {
+        return false;
+    }
+    var row = document.createElement("tr");
+    row.innerHTML = `<td>${users.length + 1}</td>
+                    <td><input type="text" id="name" class="form-control input-sm"/></td>
+                    <td><input type="text" id="surname" class="form-control input-sm"/></td>
+                    <td><input type="text" id="email" class="form-control input-sm"/></td>
+                    <td><input type="text" id="age" class="form-control input-sm"/></td>
+                    <td>
+                    <a href="#" class="btn btn-sm btn-default" id="success">
+                        <i class="far fa-check"></i>
+                    </a>
+                    <a href="#" class="btn btn-sm btn-default" id="cancel">
+                        <i class="far fa-times"></i>
+                    </a>
+                    </td>`
+    row.querySelector('#cancel').addEventListener("click", () => {
+        row.remove();
+        check = true;
+    })
+    row.querySelector('#success').addEventListener("click", () => {
+        addEntry(row);
+        check = true;
+    })
+    entries.appendChild(row);
+    check = false;
+    return true;
+})
+
+function addEntry(row) {
+    users.push(new Person(row.querySelector('#name').value, 
+        row.querySelector('#surname').value, 
+        row.querySelector('#email').value, 
+        row.querySelector('#age').value));
+    index = parseInt(row.children[0].innerHTML)-1;
+    row.innerHTML = users[index].info(index);
 }
