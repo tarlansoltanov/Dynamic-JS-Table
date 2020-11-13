74b950f032c18e909c2cf1fcf5dffb69430d8e39
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2020-11-13 22:53:15 +0400

    Comments added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var users = []
+var users = [] // For storing entries
 
 class Person {
     constructor(name, surname, email, age) {
@@ -9,6 +9,7 @@ class Person {
     }
 
     info(index) {
+        // This method will fill row with users info
         return `<td>${index+1}</td>
         <td>${this.name}</td>
         <td>${this.surname}</td>
@@ -25,6 +26,7 @@ class Person {
     }
 
     edit(name, surname, email, age){
+        // This method will edit persons info with given data
         this.name = name;
         this.surname = surname;
         this.email = email;
@@ -32,14 +34,15 @@ class Person {
     }
 }
 
-const addBtn = document.querySelector('#add');
+const addBtn = document.querySelector('#add'); // Add Button
 
-const entries = document.querySelector('tbody');
+const entries = document.querySelector('tbody'); // All entries
 
-var check = true;
+var check = false; // Checking the add row is exist or not
 
-var edit = false;
+var edit = false; // Checking the any row is editing or not
 
+// Filling table with users info
 for (var i = 0; i < users.length; i++) {
     var row = document.createElement('tr');
     row.innerHTML = users[i].info(i);
@@ -47,8 +50,11 @@ for (var i = 0; i < users.length; i++) {
     addListeners(row);
 }
 
+// Event Listener for add button
+// This will add a row with inputs to the table
 addBtn.addEventListener("click", () => {
-    if (check == false) {
+    if (check == true) {
+        // Checking add row exist or not
         return false;
     }
     var row = document.createElement("tr");
@@ -65,19 +71,22 @@ addBtn.addEventListener("click", () => {
                         <i class="far fa-times"></i>
                     </a>
                     </td>`
+    // Adding event listener for removing the row
     row.querySelector('#cancel').addEventListener("click", () => {
         removeRow(row);
-        check = true;
+        check = false;
     })
+    // Adding event listener for adding info's in row
     row.querySelector('#success').addEventListener("click", () => {
         addEntry(row);
-        check = true;
+        check = false;
     })
     entries.appendChild(row);
-    check = false;
+    check = true;
     return true;
 })
 
+// Function for removing the given row
 function removeRow(row) {
     var removedIndex = row.children[0].innerHTML;
     users.splice(removedIndex - 1,1);
@@ -85,12 +94,14 @@ function removeRow(row) {
     adjustIndices();
 }
 
+// Function for adjust indexes in table's rows
 function adjustIndices() {
     for (var i = 0; i < entries.children.length; i++) {
         entries.children[i].children[0].innerHTML = i+1;
     }
 }
 
+// Function for adding entries to the list in the given row's inputs
 function addEntry(row) {
     users.push(new Person(row.querySelector('#name').value, 
         row.querySelector('#surname').value, 
@@ -101,14 +112,13 @@ function addEntry(row) {
     addListeners(row);
 }
 
+// Function for editing etries in given row
 function editEntry(row) {
-    console.log(row);
     if(edit==true){
         return false;
     }
     edit = true;
     index = parseInt(row.children[0].innerHTML)-1;
-    console.log(index);
     row.innerHTML = `
             <td>${index + 1}</td>
             <td><input type="text" id="name" class="form-control input-sm" value="${users[index].name}"/></td>
@@ -139,6 +149,7 @@ function editEntry(row) {
     })
 }
 
+// Function for adding event listeners to given row
 function addListeners(row){
     row.querySelector('#remove').addEventListener("click", () => {
         removeRow(row);
